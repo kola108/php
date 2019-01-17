88f6856fc061c8a8ddfaa475efa9aed49ccd7657
Nikolay Shchukin <ns@digio.ch>
2019-01-17 18:12:07 +0200

    update home work 2

--- HW_2/script.php
+++ HW_2/script.php
@@ -1,7 +1,9 @@
 <?php
 
-if($_POST['userAge'] < 18){
+if(($_POST['userAge'] < 18) && (!empty($_POST))){
     echo  'Hi ' . $_POST['userName'] . ' you are young person!';
-} else {
+} elseif(($_POST['userAge'] >= 18) && (!empty($_POST))) {
     echo  'Okay ' . $_POST['userName'] . ' now lets go drink vodka!';
+} else {
+    echo "error";
 }
