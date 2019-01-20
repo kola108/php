314d794293fd4ad4141f1a8315b2c9f2678cb3a2
Nikolay Shchukin <ns@digio.ch>
2019-01-20 13:34:56 +0200

    home work 4 task 3

---
+++ HW_4/save_user_data.php
@@ -0,0 +1,23 @@
+<?php
+
+$fileName = "user.txt";
+
+if (file_exists($fileName)) {
+
+    $file = fopen($fileName, "w+") or die("can't open the file");
+
+    fputs($file, "user name: " . $_GET["userName"] . "   ");
+    fputs($file, "login: " . $_GET["login"] . "   ");
+    fputs($file, "password: " . $_GET["password"] . "   ");
+    fputs($file, "confirm password: " . $_GET["confirmPassword"] . "   ");
+    fputs($file, "email: " . $_GET["email"] . "   ");
+    fputs($file, "sex: " . $_GET["sex"] . "   ");
+    fputs($file, "birthday: " . $_GET["birthday"] . "   ");
+    fputs($file, "country: " . $_GET["country"] . "   ");
+    fputs($file, "city: " . $_GET["city"] . "   ");
+
+    echo "<p>file text:</p> <pre>" . file_get_contents($fileName) . "</pre>";
+
+} else {
+    echo "can't open the file";
+}
