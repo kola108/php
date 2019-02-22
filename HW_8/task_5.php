0a8dfd23b6ac400cb28d914060db8f413505d83f
Nikolay Shchukin <ns@digio.ch>
2019-02-22 11:35:26 +0200

    add home work 8 task 5

--- HW_8/task_5.php
+++ HW_8/task_5.php
@@ -1,2 +1,21 @@
 <?php
 
+class Session {
+    public function __construct() {
+        session_start();
+    }
+
+    public function createSessionVar($sessionVar) {
+        $_SESSION['sessionVar'] = $sessionVar;
+    }
+    public function getSessionVar() {
+        return $_SESSION['sessionVar'];
+    }
+    public function deleteSessionVar() {
+        unset($_SESSION['sessionVar']);
+    }
+
+    public function __destruct() {
+        session_destroy();
+    }
+}
